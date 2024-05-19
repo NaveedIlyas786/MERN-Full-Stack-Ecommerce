77c906423c0d2e39012b43f90dfb842c56d44ade
Naveed <naveedilyas321@gmail.com>
2024-05-19 23:26:20 +0500

    Now Email is sending also through config.env file,(point to remember) use the valid email address and use also the app password of that specific email address not use of another one Email

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,20 +3,20 @@ const nodeMailer = require("nodemailer"); //? this is default fucntion of node j
 const sendEmail = async(options)=>{
     const transporter = nodeMailer.createTransport({
 
-        service: "gmail",  //? means *simple mail transfer protocol* we shall replace this service as well as others with dynamic and flexible Mail data using "process.env" in config.env
-        host: "smtp.gmail.com",   //! If your email not send then add these two lines *Host* and *Port* and install *NodeMailer* also using npm
-        port: 465,
+        service:process.env.SMTP_SERVICE,  //? means *simple mail transfer protocol* we shall replace this service as well as others with dynamic and flexible Mail data using "process.env" in config.env
+        host:process.env.SMTP_HOST,   //! If your email not send then add these two lines *Host* and *Port* and install *NodeMailer* also using npm
+        port: process.env.SMTP_PORT,
         secure: true,
         
         auth:{
-            user:"naveedilyas321@gmail.com",
-            pass:"nezr xkve dsgl rcqj",
+            user:process.env.SMTP_MAIL,
+            pass:process.env.SMTP_PASSWORD,
         }
         
     });
 
     const mailOptions = {
-        from: "naveedilyas321@gmail.com",
+        from: process.env.SMTP_MAIL,
         to: options.email,
         subject: options.subject,
         text: options.message
